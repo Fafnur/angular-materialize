b5a6514472ffb35141a78177956c9d27161999ac
Aleksandr Serenko <fafnur@yandex.ru>
2024-12-26 22:16:40 +0700

    [input-field] Generating input field components

---
+++ src/app/home/page/lib/input-field/input-label/input-label.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-label',
+  imports: [],
+  templateUrl: './input-label.component.html',
+  styleUrl: './input-label.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class InputLabelComponent {
+
+}
---
+++ src/app/home/page/lib/input-field/input-prefix/input-prefix.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-prefix',
+  imports: [],
+  templateUrl: './input-prefix.component.html',
+  styleUrl: './input-prefix.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class InputPrefixComponent {
+
+}
