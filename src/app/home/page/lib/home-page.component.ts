dc94aabc78d0fe181d5a5305919650d3a6898ccb
Aleksandr Serenko <fafnur@yandex.ru>
2024-12-26 19:25:33 +0700

    [materialize] Adding input direct

--- src/app/home/page/lib/home-page.component.ts
+++ src/app/home/page/lib/home-page.component.ts
@@ -1,9 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import type { OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, DestroyRef, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { tap } from 'rxjs';
+
+import { InputDirectComponent } from './input-direct/input-direct.component';
 
 @Component({
   selector: 'app-home-page',
+  imports: [InputDirectComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePageComponent {}
+export class HomePageComponent implements OnInit {
+  private readonly destroyRef = inject(DestroyRef);
+
+  readonly form = new FormGroup({
+    email: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.email],
+    }),
+  });
+
+  ngOnInit(): void {
+    this.form.valueChanges.pipe(tap(console.log), takeUntilDestroyed(this.destroyRef)).subscribe();
+  }
+}
